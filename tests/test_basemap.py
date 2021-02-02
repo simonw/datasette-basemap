d6a82d8ea3de87799a24310c740cccbad1684c89
Simon Willison <swillison@gmail.com>
2021-02-02 15:32:25 -0800

    Added attribution metadata, closes #3

--- tests/test_basemap.py
+++ tests/test_basemap.py
@@ -15,3 +15,21 @@ async def test_database():
         "name": "tiles",
         "columns": ["zoom_level", "tile_column", "tile_row", "tile_data"],
     }.items() <= tiles.items()
+
+
+@pytest.mark.asyncio
+async def test_metadata():
+    datasette = Datasette([], memory=True)
+    await datasette.invoke_startup()
+    response = await datasette.client.get("/basemap/metadata.json?_shape=array")
+    metadata = {r["name"]: r["value"] for r in response.json()}
+    assert metadata == {
+        "name": "datasette-basemap 0.2",
+        "format": "png",
+        "minzoom": "0",
+        "maxzoom": "6",
+        "bounds": "-180.0,-90.0,180.0,90.0",
+        "center": "0.0,0.0,3",
+        "template": "{{#__location__}}{{/__location__}} {{#__teaser__}}          {{/__teaser__}}{{#__full__}}{{/__full__}}",
+        "attribution": "© OpenStreetMap contributors",
+    }
